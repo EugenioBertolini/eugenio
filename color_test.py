75df6b8b8f22631da883a55b2c6c5f6ad1550c8b
Eugenio Bertolini <eugenio.bertolini@mail.polimi.it>
2024-02-09 15:09:40 +0900

    .

--- color_test.py
+++ color_test.py
@@ -1,6 +1,6 @@
 import matplotlib
 
-matplotlib.use("Qt5Agg")  # Use TkAgg or TkCairo backend
+matplotlib.use("TkAgg")  # Use TkAgg or TkCairo backend
 import matplotlib.pyplot as plt
 from enum import Enum
 
